0cef5ac1ca2aab2d53d68aff99f1cbee9aa02e40
o_utkin <utkin.oleksii@gmail.com>
2023-06-20 09:06:09 +0300

    second commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 
 # Функція для створення гральної дошки
 def create_board():
-    board = [['-' for _ in range(3)] for _ in range(3)]
+    board = [['-' for i in range(3)] for j in range(3)]
     return board
 
 
